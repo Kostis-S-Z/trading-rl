57423f0e6401ac3eca0533695daa5fb2a4698db3
Kostis-S-Z <kostis.s.z.24@gmail.com>
2019-08-25 15:11:27 +0200

    Add comments following paper notation

--- trading_agent/trail_env.py
+++ trading_agent/trail_env.py
@@ -9,6 +9,7 @@ import os
 import random
 import numpy as np
 
+# Set indices for different trade actions
 BUY = 1
 NEUTRAL = 0
 SELL = -1
@@ -22,10 +23,14 @@ class Trail(Environment):
         Load data and set action and observation space
         """
 
+        # Input shape originally contains the distance between the current estimation
+        # and the upper and the lower margin of the agent.
+        # If one-hot is enabled then the input shape is automatically increased to size 5 (2 + 3 binary values)
         input_shape = 2
 
         super().__init__(folder, steps, train_data, test_data, test_starts, input_shape, **kwargs)
 
+        # Set these default values if not manual values are not given
         var_defaults = {
             "margin": 0.01,
             "turn": 0.001,
@@ -33,15 +38,14 @@ class Trail(Environment):
             "dp": False,
             "reset_margin": True,
         }
-
         for var, default in var_defaults.items():
             setattr(self, var, kwargs.get(var, default))
 
         self.observations = []
-
         self.pnls_0 = []
         self.epoch_pnl_0 = 0
 
+        # Initialize the environment
         self.reset()
 
     def step(self, action):
@@ -55,23 +59,25 @@ class Trail(Environment):
         return: the new state, reward and if the data are done
         """
 
-        value = self.data[self.position]
+        c_val = self.data[self.position]  # this is p_t
 
         if action == 2:  # sell / short
             self.action = SELL
-            self.value -= self.value * self.turn
-            self.short_actions.append([self.position, value])
+            self.value -= self.value * self.turn  # Update s(t+1) = s(t) + [s(t) * a]
+            self.short_actions.append([self.position, c_val])
             self.prev_fin_pos = SELL
         elif action == 1:  # buy / long
             self.action = BUY
-            self.value += self.value * self.turn
-            self.long_actions.append([self.position, value])
+            self.value += self.value * self.turn  # Update s(t+1) = s(t) - [s(t) * a]
+            self.long_actions.append([self.position, c_val])
             self.prev_fin_pos = BUY
-        else:
-            self.action = 0
+        else:  # stay
+            self.action = NEUTRAL  # Update s(t+1) = s(t)
 
+        # If the are still more data to process
         if (self.position + 1) < self.data_size:
-            state = [self.position, value, self.action, self.value]
+            # Save the current state
+            state = [self.position, c_val, self.action, self.value]
             self.memory.append(state)
 
             # Move the agent to the next timestep
@@ -83,11 +89,10 @@ class Trail(Environment):
 
             # If the agent gets out of boundaries reset its trade position
             if self.reset_margin:
-                # if the agent gets out of boundaries reset him
                 value = self.data[self.position]
                 upper = value + self.margin
                 lower = value - self.margin
-                if (self.value > upper or self.value < lower):
+                if self.value > upper or self.value < lower:
                     self.value = value
 
             self.observation = self.input_s()
@@ -100,6 +105,7 @@ class Trail(Environment):
         """
         After each epoch or at a start of a process (train, test, validation) reset the variables.
         """
+        # If its testing phase, save results in a different folder
         if self.testing:
             self.data = np.load(self.test_data)
             self.data_size = len(self.data)
@@ -107,21 +113,21 @@ class Trail(Environment):
             self.test_starts_index += 1
             self.test_folder = self.folder + '/Test_' + str(self.test_starts_index)
             os.makedirs(self.test_folder)
-        elif self.validation_process:
+        elif self.validation_process:  # Validation process is on different data
             self.data = np.load(self.validation_data)
             self.data_size = len(self.data)
             self.position = self.val_starts[self.val_starts_index]
             self.val_starts_index += 1
         else:
             begin = 0
-            # In case you want to limit the number of data to use in training, use this
+            # In case you want to limit the number of data to use in training, uncomment the next line
             # end = self.limit_data
             end = self.data_size - self.steps - 1
             self.position = random.randint(begin, end)
 
-        # note that the action being taken is for the position X but the input
-
+        # Save previous epoch's rewards
         self.rewards.append(self.epoch_reward)
+        # Reset all variables
         self.epoch_reward = 0
         self.memory = []
         self.long_actions = []
@@ -132,6 +138,7 @@ class Trail(Environment):
 
         self.reward = 0
 
+        # Start again from a new index
         self.value = self.data[self.position]
         self.action = 0  # action in step t
         self.prev_action = 0  # action of t-1 step
@@ -142,6 +149,7 @@ class Trail(Environment):
         self.sell_flag = False
         self.done = False
 
+        # Get the first observation vector
         self.observation = self.input_s()
 
         return self.observation
@@ -174,7 +182,7 @@ class Trail(Environment):
             a = int(self.action == BUY)
             b = int(self.action == SELL)
             c = int(self.action != BUY and self.action != SELL)
-            observation = np.append(observation, [a,b,c])
+            observation = np.append(observation, [a, b, c])
 
         return observation
 
@@ -184,27 +192,26 @@ class Trail(Environment):
         Normalize the reward to a proper range
         """
 
-        up_margin = self.data[self.position] + self.margin
-        c_val = self.data[self.position]
-        pr_val = self.data[self.position - 1]
-        down_margin = self.data[self.position] - self.margin
+        c_val = self.data[self.position]  # p(t)
+        up_margin = c_val + self.margin  # p(t) + m
+        down_margin = c_val - self.margin  # p(t) - m
 
         # Because its almost impossible to get the exact number, use an acceptable slack
         if np.abs(c_val - self.value) < 0.00001:
             reward = 1
-        elif self.value <= c_val:
-            reward = ( self.value - down_margin ) / ( c_val - down_margin )
+        elif self.value <= c_val:  # s(t) < p(t)
+            reward = (self.value - down_margin) / (c_val - down_margin)  # same as ( s(t) - p(t) + m ) / m
         else:
-            reward = ( self.value - up_margin ) / ( c_val - up_margin )
+            reward = (self.value - up_margin) / (c_val - up_margin)  # same as (p(t) - s(t) + m ) / m
 
-        if self.ce:
+        if self.ce:  # If there was a change in the financial position (trade action) of the agent, apply a fee
             if self.action != self.prev_action:
                 reward = reward - np.abs(reward * self.cost)
         else:
             if (self.action == BUY or self.action == SELL) and (self.action != self.prev_fin_pos):
                 reward = reward - np.abs(reward * self.cost)
 
-        if self.dp:
+        if self.dp:  # Add another extra penalty if the trade action was from short to long or from long to short
             if ((self.prev_action == BUY) and (self.action == SELL)) or ((self.prev_action == SELL) and (self.action == BUY)):
                 reward = reward - np.abs(reward * self.cost)
 
